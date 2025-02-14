7d35f36a3d14e7ef7c72d478c39ace9c8693b47d
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-02-14 21:12:14 +0100

    fix(main): remove unnecessary newline in main function

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ func main() {
 	cmd := exec.Command("git", "--no-pager", "diff", "--staged")
 	output, err := cmd.Output()
 	commitMessage := ai.GetCommitMessage(string(output))
-
 	if err != nil {
 		fmt.Println(err)
 		return
