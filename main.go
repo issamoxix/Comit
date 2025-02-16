3f87d1c3e1b1cc147716a15dcb987cedc7fc3d4d
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-02-16 15:10:55 +0100

    feat(cli): add command-line options for 'update' and 'version' to enhance user interaction

--- main.go
+++ main.go
@@ -2,11 +2,65 @@ package main
 
 import (
 	ai "commit_helper/services/openai"
+	"commit_helper/services/utils"
 	"fmt"
+	"net/http"
+	"os"
 	"os/exec"
+	"runtime"
+
+	"github.com/inconshreveable/go-update"
 )
 
+var UpdateLink = "https://github.com/issamoxix/Comit/releases/download/%s/%s"
+var Version = "0.2"
+
+func selfUpdate() error {
+	fmt.Println("Checking for updates...")
+	updateVersion := utils.GetVersion()
+	var fileName string
+	switch runtime.GOOS {
+	case "windows":
+		fileName = "comit.exe"
+	case "darwin":
+		fileName = "comit"
+	default:
+		return fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
+	}
+
+	latestBinaryURL := fmt.Sprintf(UpdateLink, updateVersion, fileName)
+	resp, err := http.Get(latestBinaryURL)
+	if err != nil {
+		return fmt.Errorf("failed to download update: %v", err)
+	}
+	defer resp.Body.Close()
+
+	err = update.Apply(resp.Body, update.Options{})
+	if err != nil {
+		return fmt.Errorf("failed to apply update: %v", err)
+	}
+
+	fmt.Println("Update successful!")
+	return nil
+}
+
 func main() {
+
+	if len(os.Args) > 1 && os.Args[1] == "update" {
+		err := selfUpdate()
+		if err != nil {
+			fmt.Println("Update failed:", err)
+		} else {
+			fmt.Println("Successfully updated to the latest version.")
+		}
+		return
+	}
+
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Printf("Version: %s", Version)
+		return
+	}
+
 	cmd := exec.Command("git", "--no-pager", "diff", "--staged")
 	output, err := cmd.Output()
 	commitMessage := ai.GetCommitMessage(string(output))
