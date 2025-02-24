ba98296b9d96031ad936c4eb88cc4eb3bbaf9cab
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-02-24 15:56:34 +0100

    feat: add command-line option to retrieve branch names

--- services/openai/ai.go
+++ services/openai/ai.go
@@ -16,6 +16,14 @@ type RequestData struct {
 	Code string `json:"code"`
 }
 
+type RequestBranchName struct {
+	Context string `json:"context"`
+}
+
+type BranchResponse struct {
+	Branch []string `json:"branch"`
+}
+
 func GetCommitMessage(content string) string {
 	url := "https://comit.issamcloud.com"
 
@@ -55,4 +63,46 @@ func GetCommitMessage(content string) string {
 		return "Error: " + err.Error()
 	}
 	return data.Message
+}
+
+func GetBranchNames(context string) string {
+	url := "https://comit.issamcloud.com/branch"
+	payload := RequestBranchName{
+		Context: context,
+	}
+
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return "Error: " + err.Error()
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println(err)
+		return "Error: " + err.Error()
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Error: " + resp.Status)
+		return "Error: " + resp.Status
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return "Error: " + err.Error()
+	}
+
+	var data BranchResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println(err)
+		return "Error: " + err.Error()
+	}
+
+	for _, branch := range data.Branch {
+		fmt.Println(branch)
+	}
+	return ""
 }
