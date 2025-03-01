464d0fb7fd633de00e0288711a548f9a7f4de4a1
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-01 14:05:16 +0100

    test: add unit test for version API endpoint

---
+++ services/utils/version_test.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+)
+
+type VersionResponseType struct {
+	Version string `json:"version"`
+}
+
+func TestVersionApi(t *testing.T) {
+	url := "https://comit.issamcloud.com/version"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var versionResponse VersionResponseType
+	err = json.Unmarshal(body, &versionResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if versionResponse.Version == "" {
+		t.Fatal("Version is empty")
+	}
+	if Version != versionResponse.Version {
+		t.Fatalf("Expected version %s, got %s", Version, versionResponse.Version)
+	}
+}
