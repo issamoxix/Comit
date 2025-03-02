74ab97767c807b3e7c312c0dfa1c970476a7b024
kamal <kamalkrami4@gmail.com>
2025-03-02 14:44:32 +0000

    feat(main.go,version.go): simplify update and version check logic in main function

--- main.go
+++ main.go
@@ -5,59 +5,33 @@ import (
 	"commit_helper/services/utils"
 	"commit_helper/services/utils/tools"
 	"fmt"
-	"net/http"
 	"os"
-	"runtime"
-
-	"github.com/inconshreveable/go-update"
+	"strings"
 )
 
-var UpdateLink = "https://github.com/issamoxix/Comit/releases/download/%s/%s"
-
-func selfUpdate() error {
-	fmt.Println("Checking for updates...")
-	updateVersion := utils.GetVersion()
-	var fileName string
-	switch runtime.GOOS {
-	case "windows":
-		fileName = "comit.exe"
-	case "darwin":
-		fileName = "comit"
-	default:
-		return fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
-	}
-
-	latestBinaryURL := fmt.Sprintf(UpdateLink, updateVersion, fileName)
-	resp, err := http.Get(latestBinaryURL)
-	if err != nil {
-		return fmt.Errorf("failed to download update: %v", err)
-	}
-	defer resp.Body.Close()
-
-	err = update.Apply(resp.Body, update.Options{})
-	if err != nil {
-		return fmt.Errorf("failed to apply update: %v", err)
-	}
-
-	fmt.Println("Update successful!")
-	return nil
-}
-
 func main() {
-
+	latestVersion := utils.GetLatestVersion()
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "update":
-			err := selfUpdate()
-			if err != nil {
-				fmt.Println("Update failed:", err)
+			if latestVersion == utils.Version {
+				fmt.Println("You already have the latest version.")
+				return
 			} else {
-				fmt.Println("Successfully updated to the latest version.")
+				err := utils.SelfUpdate()
+				if err != nil {
+					fmt.Println("Update failed:", err)
+				} else {
+					fmt.Println("Successfully updated to the latest version.")
+				}
 			}
 			return
 
 		case "version":
-			fmt.Printf("Version: %s", utils.Version)
+			fmt.Printf("Current Version: %s\n", utils.Version)
+			if strings.Compare(latestVersion, utils.Version) == -1 {
+				fmt.Printf("🚀 A new version (%s) is available! Please update.\n", utils.Version)
+			}
 			return
 
 		case "-b":
@@ -67,6 +41,7 @@ func main() {
 		case "help":
 			fmt.Println("Usage: comit [command]\nCommands: \n\tupdate   : To update the app\n\tversion  : To see the version of the app\n\t-b       : To generate branch name\n\thelp     : To see this help message")
 			return
+
 		default:
 			fmt.Printf("Unknown command: %s\nUse \"comit help\" to see available commands", os.Args[1])
 			return
--- services/utils/version.go
+++ services/utils/version.go
@@ -2,17 +2,25 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"runtime"
+
+	"github.com/inconshreveable/go-update"
 )
 
 type VersionResponse struct {
 	Version string `json:"version"`
 }
 
-func GetVersion() string {
-	url := "https://comit.issamcloud.com/version"
-	resp, err := http.Get(url)
+const (
+	VersionURL = "https://comit.issamcloud.com/version"
+	UpdateLink = "https://github.com/issamoxix/Comit/releases/download/%s/%s"
+)
+
+func GetLatestVersion() string {
+	resp, err := http.Get(VersionURL)
 	if err != nil {
 		return Version
 	}
@@ -35,4 +43,32 @@ func GetVersion() string {
 		return Version
 	}
 	return versionResponse.Version
+}
+
+func SelfUpdate() error {
+	fmt.Println("Checking for updates...")
+	latestVersion := GetLatestVersion()
+
+	var fileName string
+	switch runtime.GOOS {
+	case "windows":
+		fileName = "comit.exe"
+	case "darwin":
+		fileName = "comit"
+	default:
+		return fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
+	}
+
+	latestBinaryURL := fmt.Sprintf(UpdateLink, latestVersion, fileName)
+	resp, err := http.Get(latestBinaryURL)
+	if err != nil {
+		return fmt.Errorf("failed to download update: %v", err)
+	}
+	defer resp.Body.Close()
+
+	err = update.Apply(resp.Body, update.Options{})
+	if err != nil {
+		return fmt.Errorf("failed to apply update: %v", err)
+	}
+	return nil
 }
