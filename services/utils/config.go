653c33725e13d088d84ec7d7111e4f4c083a00fe
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-06 21:12:46 +0100

    fix: update ComitURL to point to the new API endpoint

--- services/utils/config.go
+++ services/utils/config.go
@@ -4,5 +4,5 @@ var Version = "0.6.5"
 
 var (
 	UpdateLink = "https://github.com/issamoxix/Comit/releases/download/%s/%s"
-	ComitURL   = "https://comit.issamcloud.com"
+	ComitURL   = "https://api.comit.dev"
 )
